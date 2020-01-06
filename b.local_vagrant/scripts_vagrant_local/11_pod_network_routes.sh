27f3ad8830caad2cbd45904bfdce712a7b569fba
Adi <aneci.adrian@gmail.com>
2020-01-06 18:14:00 +0200

    update network routes for b option(local vagrant)

--- b.local_vagrant/scripts_vagrant_local/11_pod_network_routes.sh
+++ b.local_vagrant/scripts_vagrant_local/11_pod_network_routes.sh
@@ -1,80 +1,11 @@
 #!/usr/bin/env bash
 
-vagrant ssh master -c '''
-cat <<EOF | tee add_routes.sh >/dev/null
-#!/bin/bash
-sudo modprobe br_netfilter
-# add containers subnets in route table
-sudo ip route add 10.200.0.0/24 via 10.240.0.20
-sudo ip route add 10.200.1.0/24 via 10.240.0.21
-# add services IP subnet in route table
-sudo ip route add 10.32.0.0/24 via 10.240.0.20
-EOF
-
-chmod +x add_routes.sh
-./add_routes.sh
-
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo \"@reboot /home/vagrant/add_routes.sh\" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
-
-cat <<EOF | sudo tee --append /etc/hosts >/dev/null
+for instance in master worker-0 worker-1; do
+    vagrant ssh ${instance} -c '''
+    cat <<EOF | sudo tee --append /etc/hosts >/dev/null
 10.240.0.20 worker-0
 10.240.0.21 worker-1
 10.240.0.10 master
 EOF
 '''
-
-vagrant ssh worker-0 -c '''
-cat <<EOF | tee add_routes.sh >/dev/null
-#!/bin/bash
-sudo modprobe br_netfilter
-sudo ip route add 10.200.1.0/24 via 10.240.0.21
-EOF
-
-chmod +x add_routes.sh
-./add_routes.sh
-
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo \"@reboot /home/vagrant/add_routes.sh\" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
-
-cat <<EOF | sudo tee --append /etc/hosts >/dev/null
-10.240.0.20 worker-0
-10.240.0.21 worker-1
-10.240.0.10 master
-EOF
-'''
-
-vagrant ssh worker-1 -c '''
-cat <<EOF | tee add_routes.sh >/dev/null
-#!/bin/bash
-sudo modprobe br_netfilter
-sudo ip route add 10.200.0.0/24 via 10.240.0.20
-EOF
-
-chmod +x add_routes.sh
-./add_routes.sh
-
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo \"@reboot /home/vagrant/add_routes.sh\" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
-
-cat <<EOF | sudo tee --append /etc/hosts >/dev/null
-10.240.0.20 worker-0
-10.240.0.21 worker-1
-10.240.0.10 master
-EOF
-'''
+done
